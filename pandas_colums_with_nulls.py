13ebcb2cc4c1a8cbecf49254e4ceb1106bb29452
kalyan <kalyan.solasa@aviso.com>
2018-10-03 11:26:54 +0530

    fetcing all colums having null values

---
+++ pandas_colums_with_nulls.py
@@ -0,0 +1,11 @@
+import pandas as pd
+data = [['john',10, 'M'],['doe',None, 'M'],['jessy',13, 'F'], ['visu', 11]]
+df = pd.DataFrame(data,columns=['Name','Age', 'Gender'])
+print('##'*20)
+print('data frame is:')
+print('##'*20)
+print(df)
+
+print(df.columns[df.isnull().any()].tolist())
+
+
